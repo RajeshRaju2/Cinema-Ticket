d28aab968ea4298218203619836344152c3e0c82
RajeshRaju1 <rajeshbandi5789@gmail.com>
2021-07-30 21:53:21 +0530

    Interface added of cinema ticket

---
+++ cinemaTicketBooking/src/com/mph/controller/CinemaTicketInterface.java
@@ -0,0 +1,15 @@
+package com.mph.controller;
+
+
+
+public interface CinemaTicketInterface{
+	public List addEmployee();
+
+	public void viewMovies(List elist);
+
+	public void addMovies(List elist);
+
+	public void bookyourSeat(List elist);
+
+	
+}
