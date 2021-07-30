df3cc16ee8273e46db83a09255cf3e944482b1ac
RajeshRaju2 <rajeshbandi2180@gmail.com>
2021-07-30 23:34:48 +0530

    Added controller files

---
+++ cinemaTicketBooking/src/com/mph/controller/CinemaTicketInterface.java
@@ -0,0 +1,13 @@
+package com.mph.controller;
+
+import java.util.List;
+
+public interface CinemaTicketInterface{
+	public List addEmployee();
+
+	public void addTicket(List elist);
+        puclic void getCinema(List elist);
+//my interfce
+
+	
+}
