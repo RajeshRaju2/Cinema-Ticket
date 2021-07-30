cf6e8b416f5bbe010b37eb04ecf0881d1d3ac809
Rishabh <Rishabhsharma88.rs@gmail.com>
2021-07-30 22:44:18 +0530

    cvs

---
+++ cinemaTicketBooking/src/com/mph/controller/Controller.java
@@ -0,0 +1,28 @@
+
+package com.mpg.controller;
+
+import java.util.*;
+//import com.mph.Exception.*;
+
+
+
+
+
+
+public class EmployeeController implements  EmplyoeeInterface  {
+
+	
+	private cid; // cinema id
+	pr
+
+	public void AddId()
+	{
+		
+		
+		
+		
+	}
+	public void getCinema()
+	{
+		
+	}
